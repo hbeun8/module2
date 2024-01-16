be30e6519b7efcd28d5bb4e032e8fa3bb8f61a08
Varun upta <n8gupta@gmail.com>
2024-01-16 07:53:00 +0000

    Final Commit

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -1,78 +1,90 @@
 //The website allows you to plan your journey with cycle and compare with other modes of transport. 
-
-
-
-//We want to load default journey for cycle on start up! 
 //Add startup code
-//Listen to event listners, such as button click events and inputs cell input
 document.addEventListener("DOMContentLoaded", function(){
+    //set focus
+    document.getElementById("where-from").focus()
+    //set defaults
+    searchPostcodeA(document.getElementById("where-from").value = "W9 3RG");
+    searchPostcodeB(document.getElementById("where-to").value = "N8 9QR");
+
     //gets all buttons
     let buttons = document.getElementsByTagName("button");
     //gets all input cells
     let inputcells = document.getElementsByTagName("input");
-    
-    // -> to be removed - list of all tags: https://www.geeksforgeeks.org/html-tags-a-to-z-list/
-
-    //Alert box appears when data-type = submit else it shows traveltype kicks of planJourney function
+        
+    //Alert box appears when data-type = Geo else it shows traveltype kicks of planJourney function
     for (let button of buttons){
         button.addEventListener("click", function(){
-            if(this.getAttribute("data-type") === "submit"){
-                alert("You clicked submit!"); //replace this with API for wait times
+            if(this.getAttribute("data-type") === "Geo"){
+                alert("You are searching for postcode."); //replace this with API for wait times
             } else{
                 let travelType= this.getAttribute("data-type");
-                alert(`You clicked ${travelType}` ); // remove this when appropriate
-                //planJourney(travelType); 
+                alert(`You clicked ${travelType}` );
             }
         });
     }
-
-    // remove this or we need it for searching inout box. 
-    for (let cell of inputcells){
-        cell.addEventListener("click", function(){
-            if(this.getAttribute("type") === "text"){
-                alert("You clicked TextCell!");
-            } else{
-                let inputType= this.getAttribute("type");
-                alert(`You clicked ${inputType}` );
-                //planJourney(travelType);
-            }
-        });
-    }
+    
 });
 
-//-> rmeove thhis Events =  interesting thing!
-//- > remove thisList of Events https://developer.mozilla.org/en-US/docs/Web/Events
+//Displays time
+function displayTime(){
+    var now = new Date();
+    var elt = document.getElementById("digital-clock");
+    elt.innerHTML = now.toLocaleTimeString();
+    //repeat in one second
+    setTimeout(displayTime, 1000);
+}
 
-/*Appends an event listener for events whose type attribute value is type. The callback argument sets the callback that will be invoked when the event is dispatched.
+//start the clock when the document loads
+window.onload = displayTime;
 
-The options argument sets listener-specific options. For compatibility this can be a boolean, in which case the method behaves exactly as if the value was specified as options's capture.
-
-When set to true, options's capture prevents callback from being invoked when the event's eventPhase attribute value is BUBBLING_PHASE. When false (or not present), callback will not be invoked when event's eventPhase attribute value is CAPTURING_PHASE. Either way, callback will be invoked if event's eventPhase attribute value is AT_TARGET.
-
-When set to true, options's passive indicates that the callback will not cancel the event by invoking preventDefault(). This is used to enable performance optimizations described in § 2.8 Observing event listeners.
-
-When set to true, options's once indicates that the callback will only be invoked once after which the event listener will be removed.
-
-If an AbortSignal is passed for options's signal, then the event listener will be removed when signal is aborted.
-
-The event listener is appended to target's event listener list and is not appended if it has the same type, callback, and capture.
-
-MDN Reference*/
-
-async function searchPostcode(postcode_in) {
+async function searchPostcodeA(postcode_in) {
 
     let postcode = postcode_in
     const url = `https://api.postcodes.io/postcodes/${postcode}`;
     fetch(url)
     .then(response => {
         if (!response.ok) {
+            alert (`Unknown Postcode: ${postcode}`);
+            throw new Error('Network response was not ok');            
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        displaydataA(data);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+//If an AbortSignal is passed for options's signal, then the event listener will be removed when signal is aborted.
+
+// this bit of code, displays geo location. 
+function displaydataA(data) {
+    const locA = data.result.postcode;
+    const outA = document.getElementById("outputA");
+    outA.innerHTML = locA;
+
+}
+
+
+async function searchPostcodeB(postcode_in) {
+
+    let postcode = postcode_in
+    const url = `https://api.postcodes.io/postcodes/${postcode}`;
+    fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            alert (`Unknown Postcode: ${postcode}`);
             throw new Error('Network response was not ok');
         }
         return response.json();
     })
     .then(data => {
         console.log(data);
-        displaydata(data);
+        displaydataB(data);
     })
     .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
@@ -81,167 +93,104 @@ async function searchPostcode(postcode_in) {
 
 
 // this bit of code, displays geo location. 
-function displaydata(data) {
-    const locA = data.result.latitude;
+function displaydataB(data) {
+    const locB = data.result.postcode;
     const outA = document.getElementById("outputA");
-    outA.innerHTML = locA;
+    const outB = document.getElementById("outputB");
+    outB.innerHTML = locB;
+    resultsMessage = `Your options for the journey from ${outA.innerHTML} to ${outB.innerHTML}`;
+    document.getElementById("resultsMessage").innerHTML = resultsMessage;
 }
 
-const geoA = document.getElementById("where-to").value;
-
-//document.getElementById("findgeoA").addEventListener("click", searchPostcode(geoA));
-
-//document.getElementById("findgeoA").addEventListener("click", searchPostcode("e14"));
-
-/*
-const geoA = document.getElementById("where-to");
-const btnA = document.getElementById("findgeoA");
-const outA = document.getElementById("outputA");
-outA.innerHTML = btnA.addEventListener("click",geoA => searchPostcode(geoA));
-
-document.getElementById("outputA").innerHTML = searchPostcode("n8 9Qr")
-// this bit of code, displays geo location when the button is clicked. 
-const geoB = docume nt.getElementById('where-from');
-const btnB = document.getElementById('findgeoB');
-const outB = document.getElementById('outputB');
-outB.innerHTML = btnB.addEventListener('click',searchPostcode(geoB));
-*/
-
-/*
-function planJourney(travelType) {
+function planJourney(from, to, mode){
+    
     //this creates an empty results area.
-    document.getElementById("result area").value= "";    document.getElementById("where-to").focus();
+//    document.getElementById("result area").value= "";    
 
+    switch (mode) {
+        case "Cycle":
+            url = `https://api.tfl.gov.uk/Journey/JourneyResults/${from}/to/${to}?accessibilityPreference=NoRequirements&walkingSpeed=Slow&cyclePreference=AllTheWay&bikeProficiency=Moderate`
+            break;
+        case "Bus":
+            url = `https://api.tfl.gov.uk/Journey/JourneyResults/${from}/to/${to}?timeIs=Departing&journeyPreference=LeastInterchange&mode=Bus&accessibilityPreference=NoRequirements&walkingSpeed=Slow&cyclePreference=None&bikeProficiency=Easy`
+            break;
+        case "Tube":
+            url = `https://api.tfl.gov.uk/Journey/JourneyResults/${from}/to/${to}?timeIs=Departing&journeyPreference=LeastInterchange&mode=Tube&accessibilityPreference=NoRequirements&walkingSpeed=Slow&cyclePreference=None&bikeProficiency=Easy`
+            break;
+        case "Walk":
+            url = `https://api.tfl.gov.uk/Journey/JourneyResults/${from}/to/${to}?timeIs=Departing&journeyPreference=LeastWalking&mode=Walk&accessibilityPreference=NoRequirements&walkingSpeed=Average&cyclePreference=None&bikeProficiency=Easy&walkingOptimization=True`
+            break;
+
+    }
+
+    fetch(url, {
+        method: 'GET',
+        // Request headers
+        headers: {
+            'Cache-Control': 'no-cache',}
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return (response.json());
+    })
+    .then(data => {
+        console.log(data);
+        displayJourney(data.journeys, mode);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+function displayJourney (data, mode) {
     //this function will be call for each travel mode.
     //Create variables for the journey
-    let pointA = document.getElementById('where-to');
-    let pointB = document.getElementById('where-from');
-    let t0 = document.getElementById('Departure');
-    let t1 = document.getElementById('Arrival');
-    //If they are valid then run the rest of this function 
-    if (travelType === "Cycle"){
-        document.getElementById('travelType').textContent = 'Cycle';
-        function showCycleDetails([pointA, pointB, t0, t1]);        
-    } else if (travelType === "Bus"){
-        document.getElementById('travelType').textContent = 'Bus';
-        function showBusDetails([pointA, pointB, t0, t1]);        
-    } else if (travelType === "Tube"){
-        document.getElementById('travelType').textContent = 'Tube';
-        function showTubeDetails([pointA, pointB, t0, t1]);        
-    } else if (travelType === "Walk"){
-        document.getElementById('travelType').textContent = 'Walk';
-        function showWalkDetails([pointA, pointB, t0, t1]);        
-    } else if (travelType === "Combo"){
-        document.getElementById('travelType').textContent = 'Combo';
-        function showTubeDetails([pointA, pointB, t0, t1]);        
+    switch (mode) {
+        case "Cycle":
+            showCycleDetails(data);
+            break;
+        case "Bus":
+            showBusDetails(data);
+            break;
+        case "Tube":
+            showTubeDetails(data);
+            break;
+        case "Walk":
+            showWalkingDetails(data);
+            break;
+        default:
+            alert (`Unknown travel type`);
+            throw `Unknown travel type. Aborting!`
+    }
+}
+
+
+
+function showBusDetails(data) {
+    document.getElementById("busDuration").innerHTML=``;
+    const busOptions = data.length;
+    document.getElementById("busRoutes").innerHTML = `There are ${busOptions} possible routes:`;
+    document.getElementById("busInterchange").innerHTML = `There require ${data[0].legs.length} interchange.`;
+    for (i=0;i<busOptions;i++){
+        document.getElementById("busDuration").innerHTML += `Routes #${i+1} takes:${parseInt(data[i].duration)} minutes.`;
     }
 
-    else {
-        alert (`Unknown travel type: $(travelType)`);
-        throw `Unknown travel type: $(travelType). Aborting!` ;  
-    }
-    }
-
- // this function will set the departure time
-function searchDeparture() {
-    const d = new Date();
-    document.getElementById("demo").innerHTML = d;
+}
+function showTubeDetails(data) {
+    document.getElementById("tubeDistance").innerHTML = data["Distance"];
+    document.getElementById("tubeDuration").innerHTML = data["Duration"];
+    document.getElementById("tubeInterchange").innerHTML = data["Interchange"];
 }
 
-// this fucntion will set the apprival time, but optional
-function searchArrival() {
-    const d = new Date();
-    document.getElementById("demo").innerHTML = d;
+function showWalkingDetails(data) {
+    document.getElementById("walkDistance").innerHTML = data["Distance"];
+    document.getElementById("walkDuration").innerHTML = data["Duration"];
+    document.getElementById("walkInterchange").innerHTML = data["Interchange"];
 }
-
-// this function checks the inputs are valid for t0,t1, pointA and pointB, an if not correct returns to default answer. 
-//gets the parameters of the journey planner  
-// no parameter
-function checkValidInputs() {
-    let pointA = document.getElementById("pointA").innerText;
-    let pointB = document.getElementById("pointB").innerText;
-    let t0 = document.getElementById("t0").innerText;
-    let t1 = document.getElementById("t1").innerText;
-    let travelType = document.getElementById("travelType").innerText;
-
-    let isCorrectpointA = pointA ==='Valid Latitude/Longitude';
-    let isCorrectpointB = pointB === 'Valid Latitude/Longitude';
-    let isCorrectt0  = t0 === 'Valid Date/Time';
-    let isCorrectt1  = t1 ==='Valid Date/Time';
-
-    document.getElementById("t0").innerText = t1<t0 ? t1: t0;
-
-    if (isCorrectpointA)) {
-        if (isCorrectpointB){
-            if (isCorrectt0) {
-                if (isCorrectt1) {
-                    alert ("Hey you got this right!:D");
-                    IncrementScore();
-                    return[pointA, pointB, t0, t1];
-                }
-            }
-        }
-    }
-        //write a code if return is an error
-    else {
-            alert (`One of the inputs is not correct $(travelType)`);
-            throw `Unknown travel type: $(travelType). Aborting!` ;
-            IncrementWrongeAnswer();
-    }
-    
-
-
-function showBusDetails() {
-    document.getElementById('pointA-C').textContent = pointA;
-    document.getElementById('pointB-C').textContent = pointB;
-    document.getElementById('t0-C').textContent = t0;
-    document.getElementById('t1-C').textContent = t1;
-    //api results....
+function showCycleDetails(data) {
+document.getElementById("cycleDistance").innerHTML = data.distance;
+document.getElementById("cycleDuration").innerHTML = data.duration;
+document.getElementById("cycleInterchange").innerHTML = 'Not applicable';
 }
-function showTubeDetails() {}
-
-
-//this will be the default journey planner on startup.
-function showCycleDetails(pointA, pointB, t0, t1) {
-    document.getElementById('pointA-C').textContent = pointA;
-    document.getElementById('pointB-C').textContent = pointB;
-    document.getElementById('t0-C').textContent = t0;
-    document.getElementById('t1-C').textContent = t1;
-    //api results....
-}
-
-function showWalkingDetails() {
-    document.getElementById('pointA-C').textContent = pointA;
-    document.getElementById('pointB-C').textContent = pointB;
-    document.getElementById('t0-C').textContent = t0;
-    document.getElementById('t1-C').textContent = t1;
-    //api results....
-}
-function showComboDetails() {
-}
-function displayTierCoverage() {}
-function displayLimeCoverage() {}
-function displaySantandaerCoverage() {}
-function displayHuFoCovergae() {}
-function displayDottCoverage() {}
-function IncrementScore() {
-    let oldScore = parseInt(document.getElementById("score").innerText);
-    //++ increments by 1.
-    //++ before adds 
-    /*the double plus or compound addition operator  basically means to add one to the value.  */
-    /* If you  put the double plus signs after the variable then JavaScript will do something like this.  It will get the ID of score and it will set  
-the inner text to the old score variable  and then it will add one to old score.
-The result is that we never see the score being  updated.*/
-/*dom before it has had one added to it. Putting  the double plus signs before the variable means  
-that JavaScript will get the ID of score,  then set the inner text to one plus old score.  
-So we see our score being updated. It's just  an interesting little thing that sometimes you  */
-
-/*
-
-document.getElementById("score").innerText = ++oldScore;
-}
-function IncrementWrongeAnswer() {
-    let oldScore = parseInt(document.getElementById("incorrect").innerText);
-    document.getElementById("incorrect").innerText = ++oldScore;
-}
-*/
